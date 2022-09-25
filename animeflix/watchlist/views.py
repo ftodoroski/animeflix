4298a1402f4b826a50e02b7942f7b9075720c6fb
Filip Todoroski <ftodoroski@ymail.com>
2022-09-25 19:31:01 -0400

    changed the params name in watchlist ListProfileWatchlistView to profile_id

--- animeflix/watchlist/views.py
+++ animeflix/watchlist/views.py
@@ -11,12 +11,12 @@ class ListProfileWatchlistView(ListAPIView):
     queryset = Watchlist.objects.all()
 
     def get(self, request, *args, **kwargs):
-        query_param = request.query_params.get('profileID', None)
+        query_param = request.query_params.get('profile_id', None)
 
         if query_param is not None:
             queryset = Watchlist.objects.filter(profile_id=query_param)
             serializer = DetailProfileWatchlistSerializer(queryset, many=True)
             return Response(data=serializer.data, status=status.HTTP_200_OK)
         
-        data = {'msg': 'No profileID parameter added'}
+        data = {'msg': 'No profile_id parameter added'}
         return Response(data=data, status=status.HTTP_400_BAD_REQUEST)
